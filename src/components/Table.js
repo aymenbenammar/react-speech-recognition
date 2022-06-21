348d489b8e0b23e6b773feca7f6c69ac02a073bd
Aymen Ben Ammar <ABenAmmar@se.linedata.com>
2022-06-21 10:00:32 +0100

    added webSockets

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,11 +2,12 @@ import { useState } from "react";
 import mockdata from "../data.json";
 import TableBody from "./TableBody";
 import TableHead from "./TableHead";
+import { Button,Form,Row, Col,InputGroup,FormControl } from 'react-bootstrap';
 
 const annyang = require('annyang');
 const Table = () => {
   const [tableData, setTableData] = useState(mockdata);
-
+  const [focus,setFocus]=useState("");
   const columns = [
     { label: "Full Name", accessor: "full_name", sortable: true },
     { label: "Email", accessor: "email", sortable: false },
@@ -15,8 +16,37 @@ const Table = () => {
     { label: "Start date", accessor: "start_date", sortable: true },
   ];
   const stop= () =>{
-    annyang.abort(); 
-    console.log("stopped");
+    annyang.pause(); 
+    console.log("paused");
+  }
+  const resume = ()=>{
+    annyang.resume();
+  }
+  const [bids, setBids] = useState([0]);
+
+  const ws = new WebSocket("wss://ws.bitstamp.net");
+
+  const api = {
+    event: "bts:subscribe",
+    data: { channel: "order_book_eurusd" },
+  };
+  
+  ws.onopen = (event) => {
+    ws.send(JSON.stringify(api));
+   
+  };
+  ws.onmessage = function (event) {
+    const json = JSON.parse(event.data);
+    try {
+      if ((json.event = "data")) {
+        setBids(json.data.bids.slice(0,1));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  ws.onclose = function (event){
+    ws.close()
   }
   const hear = () => {
     if(annyang){
@@ -32,6 +62,29 @@ const Table = () => {
             'Start date up.' :function(){handleSorting("start_date","asc")},
             'Start date down.' : function() {handleSorting("start_date","desc")},
             'Scroll to *section': function(tag){console.log(tag)},
+            'Mail.': function(){document.getElementById('e-mail').focus();
+                                setFocus('e-mail');
+                                annyang.trigger('You just focused on mail');
+                                },
+            'Password.':function(){document.getElementById('password').focus();
+            setFocus('password');},
+
+            'Set :field to *text' : function(field,text){document.getElementById(field).value=text.slice(0,-1)},
+            'Euro to Dollar?': function(){
+              api.data.channel="order_book_eurusd";
+              ws.close()
+              ws.send(JSON.stringify(api));
+              console.log(api)
+              document.getElementById('firstamount').innerHTML="€";
+              document.getElementById('secondamount').innerHTML="$";
+            },
+            'Bitcoin to Dollar?': function(){
+              api.data.channel="order_book_btcusd";
+               
+              ws.send(JSON.stringify(api));
+              document.getElementById('firstamount').innerHTML="BTC";
+              document.getElementById('secondamount').innerHTML="$";
+            }
 
         }
         
@@ -67,14 +120,53 @@ annyang.addCallback('soundstart', function() {
   };
 
   return (
-    <>
+    <>                  
       <section id="section one">
         <table className="table">
      <TableHead {...{ columns, handleSorting }} />
      <TableBody {...{ columns, tableData }} />
    </table></section>
-      <button onClick={hear}>Start</button>
-      <button onClick={stop}>Stop</button>
+      <Form>
+      <Row className="mb-3">
+    <Col>
+      <Form.Control placeholder="E mail" id="e-mail" />
+    </Col>
+    <Col>
+      <Form.Control placeholder="Password" id="password" />
+    </Col>
+  </Row>
+  <Row className="mb-3">
+    <Col>
+    <InputGroup className="mb-3">
+    <InputGroup.Text id="firstamount">€</InputGroup.Text>
+    <FormControl aria-label="Amount (to the nearest dollar)" value="1" />
+  </InputGroup>
+    </Col>
+    <Col>
+    <InputGroup className="mb-3">
+    <InputGroup.Text id="secondamount">$</InputGroup.Text>
+    <FormControl aria-label="Amount (to the nearest dollar)" value={bids} />
+  </InputGroup>
+    </Col>
+  </Row>
+  <Row className="mb-3">
+    <Col>
+  <Button variant="primary" onClick={hear}>
+    Start Voice Assistant
+  </Button>
+  </Col>
+  <Col>
+  <Button variant="primary" onClick={resume}>
+    Resume
+  </Button>
+  </Col>
+  <Col>
+  <Button variant="primary" onClick={stop}>
+    Pause
+  </Button>
+  </Col>
+  </Row>
+</Form>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
  <React.StrictMode>
